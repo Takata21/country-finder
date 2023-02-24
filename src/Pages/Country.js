ecc666efbca27d12f3fcac5b8871fef0ddc53e84
Manuel Takata <takata211011@gmail.com>
2023-02-23 23:22:20 -0500

    update loading handler

--- src/Pages/Country.js
+++ src/Pages/Country.js
@@ -1,85 +1,88 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { HiOutlineArrowNarrowLeft } from 'react-icons/hi';
-import { useParams } from 'react-router-dom';
-import { API_URL_V2, fetchData } from '../Api/FetchData';
-import Spinner from '../components/Spinner';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { HiOutlineArrowNarrowLeft } from 'react-icons/hi'
+import { useParams } from 'react-router-dom'
+import { API_URL_V2, fetchData } from '../Api/FetchData'
+import Spinner from '../components/Spinner'
 const Country = () => {
-  const params = useParams();
-  const [isLoading, setIsLoading] = useState(false);
-  const [country, setCountry] = useState('');
+  const params = useParams()
+  const [isLoading, setIsLoading] = useState(false)
+  const [country, setCountry] = useState('')
   useEffect(() => {
     const getCountry = async () => {
-      setIsLoading(true);
-      const response = await fetchData(`${API_URL_V2}/alpha/${params.alpha}`);
-      setCountry(response.data);
-      setIsLoading(false);
-    };
-    getCountry();
-  }, [params]);
+      setIsLoading(true)
+      const response = await fetchData(`${API_URL_V2}/alpha/${params.alpha}`)
+      setCountry(response.data)
+      setIsLoading(false)
+    }
+    getCountry()
+  }, [params])
 
   return (
     <>
       {isLoading ? (
         <Spinner />
       ) : (
-        <div className="w-11/12 m-auto  dark:text-WhiteT">
+        <div className="w-11/12 m-auto dark:text-WhiteT">
           <Link
             to="/"
-            className="flex items-center justify-center p-1 my-7 gap-2  shadow-md shadow-DarkGrayTs hover:shadow-DarkGrayT transition-all duration-500 dark:bg-DarkBlueT dark:text-WhiteT xl:text-xl md:w-36"
+            className="flex items-center justify-center gap-2 p-1 transition-all duration-500 shadow-md my-7 shadow-DarkGrayTs hover:shadow-DarkGrayT dark:bg-DarkBlueT dark:text-WhiteT xl:text-xl md:w-36"
           >
             <HiOutlineArrowNarrowLeft />
             Back
           </Link>
           <div className="xl:flex xl:justify-around xl:items-center ">
             <img
-              src={country.flag}
+              src={country?.flag}
               alt=""
-              className="w-full object-fill md:max-w-xs md:h-48 xl:max-w-lg h-96 xl:object-fill xl:items-center"
+              className="object-fill w-full md:max-w-xs md:h-48 xl:max-w-lg h-96 xl:object-fill xl:items-center"
             />
             <div className="">
               <div className="">
-                <h6 className="my-6 font-extrabold text-lg xl:my-0">
-                  {country.name}
+                <h6 className="my-6 text-lg font-extrabold xl:my-0">
+                  {country?.name}
                 </h6>
                 <div className="">
                   <div className="">
-                    <p className="font-bold my-1">
+                    <p className="my-1 font-bold">
                       Native Name:
-                      <span className="font-normal"> {country.nativeName}</span>
+                      <span className="font-normal">
+                        {' '}
+                        {country?.nativeName}
+                      </span>
                     </p>
-                    <p className="font-bold my-1">
+                    <p className="my-1 font-bold">
                       Population:
                       <span className="font-normal"> {country.population}</span>
                     </p>
-                    <p className="font-bold my-1">
+                    <p className="my-1 font-bold">
                       Region:
-                      <span className="font-normal"> {country.region}</span>
+                      <span className="font-normal"> {country?.region}</span>
                     </p>
-                    <p className="font-bold my-1">
+                    <p className="my-1 font-bold">
                       Sub Region:
-                      <span className="font-normal">{country.subregion}</span>
+                      <span className="font-normal">{country?.subregion}</span>
                     </p>
-                    <p className="font-bold my-1">
+                    <p className="my-1 font-bold">
                       Capital:
-                      <span className="font-normal"> {country.capital}</span>
+                      <span className="font-normal"> {country?.capital}</span>
                     </p>
                   </div>
                   <div className="">
-                    <p className="font-bold my-1">
+                    <p className="my-1 font-bold">
                       Tp Level Domain: <span className="font-normal">be</span>
                     </p>
-                    <p className="font-bold my-1">
+                    <p className="my-1 font-bold">
                       Currencies:{' '}
                       <span className="font-normal">
                         {' '}
-                        {country.currencies[0]?.name}
+                        {country?.currencies[0]?.name}
                       </span>
                     </p>
-                    <div className="flex items-center gap-2 font-bold my-1">
+                    <div className="flex items-center gap-2 my-1 font-bold">
                       <p>Language:</p>
                       {country.languages?.map((language, index) => {
-                        return <p className="font-normal">{language.name}</p>;
+                        return <p className="font-normal">{language?.name}</p>
                       })}
                     </div>
                   </div>
@@ -87,12 +90,12 @@ const Country = () => {
               </div>
               <div className="mb-4 md:flex md:items-center md:gap-4">
                 <p className="text-lg font-bold"> Border Countries:</p>
-                <div className="flex justify-evenly  xl:items-center ">
+                <div className="flex justify-evenly xl:items-center ">
                   {country.borders?.map((border, index) => (
                     <Link
                       key={index}
                       to={`/country/${border}`}
-                      className="flex items-center justify-center p-1  gap-2  shadow-md shadow-DarkGrayTs hover:shadow-DarkGrayT transition-all duration-500 dark:bg-DarkBlueT dark:text-WhiteT"
+                      className="flex items-center justify-center gap-2 p-1 transition-all duration-500 shadow-md shadow-DarkGrayTs hover:shadow-DarkGrayT dark:bg-DarkBlueT dark:text-WhiteT"
                     >
                       {border}
                     </Link>
@@ -104,7 +107,7 @@ const Country = () => {
         </div>
       )}
     </>
-  );
-};
+  )
+}
 
-export default Country;
+export default Country
--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 const CountryCard = ({ country }) => {
   return (
     <Link to={`/country/${country.cca3}`} className="flex flex-col">
-      <article className="w-60 m-auto pb-6 shadow-md shadow-DarkGrayTs dark:bg-DarkBlueT dark:text-WhiteT hover:  hover:shadow-DarkGrayT transition-all duration-500 rounded overflow-hidden">
+      <article className="pb-6 m-auto overflow-hidden transition-all duration-500 rounded shadow-md w-60 shadow-DarkGrayTs dark:bg-DarkBlueT dark:text-WhiteT hover: hover:shadow-DarkGrayT">
         <img
           src={country.flags[0]}
-          alt={`flag of ${country.name.common}`}
-          className=" w-full h-32 xl:h-40 object-cover"
+          alt={`flag of ${country?.name?.common}`}
+          className="object-cover w-full h-32 xl:h-40"
           loading="lazy"
         />
         <div className="pl-3">
-          <p className="capitalize my-4 font-bold xl:my-5">
-            {country.name.common}
+          <p className="my-4 font-bold capitalize xl:my-5">
+            {country?.name?.common}
           </p>
-          <p className="capitalize font-semibold ">
+          <p className="font-semibold capitalize ">
             Population:{' '}
             <span className="font-normal">
-              {new Intl.NumberFormat('en-Us').format(country.population)}
+              {new Intl.NumberFormat('en-Us').format(country?.population)}
             </span>
           </p>
-          <p className="capitalize font-semibold">
-            Region: <span className="font-normal">{country.region}</span>
+          <p className="font-semibold capitalize">
+            Region: <span className="font-normal">{country?.region}</span>
           </p>
-          <p className="capitalize font-semibold">
+          <p className="font-semibold capitalize">
             Capital:{' '}
             <span className="font-normal">
-              {country.capital ? country.capital[0] : 'does not have capital'}
+              {country?.capital ? country?.capital[0] : 'does not have capital'}
             </span>
           </p>
         </div>
       </article>
     </Link>
-  );
-};
+  )
+}
 
-export default CountryCard;
+export default CountryCard
